976575d4298b2f3373823df911aad73f28328686
Basit Javed <basit.javed@gmail.com>
2021-07-11 20:05:58 +0200

    updated the app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component.js';
+import ShopPage from './pages/shop/shop.component.js';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 function App() {
   return (
     <div>
       <Switch>
-        <Route path='/shop/hats' component={HatsPage} />
         <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import './directory.styles.scss';
 import MenuItem from '../menu-item/menu-item.component';
 
-class Directory extends Component {
+export default class Directory extends Component {
   constructor() {
     super();
-
     this.state = {
       sections: [
         {
@@ -47,18 +46,10 @@ class Directory extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
-          <MenuItem
-            key={id}
-            title={title}
-            imageUrl={imageUrl}
-            size={size}
-            linkUrl={linkUrl}
-          />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
   }
 }
-
-export default Directory;
